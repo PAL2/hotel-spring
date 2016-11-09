89ca9f1af6fe6ce1b2162cb45c48289f3743272c
Alex Polegoshko <pal16@tut.by>
2016-11-09 20:06:08 +0300

    fixed Controller and web.xml

--- web/src/main/java/com/hotel/servlet/Controller.java
+++ web/src/main/java/com/hotel/servlet/Controller.java
@@ -17,8 +17,9 @@ import java.io.IOException;
 /**
  * Servlet implementation class Controller
  */
+@WebServlet("/controller")
 @org.springframework.stereotype.Controller
-@RequestMapping (value = "/")
+//@RequestMapping (value = "/")
 public class Controller extends HttpServlet {
 
     /**
