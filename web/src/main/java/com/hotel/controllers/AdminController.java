5d403d3a28cb13e97b8286b77474f8d07dca32e7
Alex Polegoshko <pal16@tut.by>
2016-11-13 18:33:46 +0300

    Redone command Allbooking

--- web/src/main/java/com/hotel/controllers/AdminController.java
+++ web/src/main/java/com/hotel/controllers/AdminController.java
@@ -1,5 +1,7 @@
 package com.hotel.controllers;
 
+import com.hotel.command.ConfigurationManager;
+import com.hotel.command.MessageManager;
 import com.hotel.entity.Booking;
 import com.hotel.service.BookingService;
 import com.hotel.service.exceptions.ServiceException;
@@ -23,8 +25,30 @@ public class AdminController {
 
     @RequestMapping(value = "/newbooking", method = RequestMethod.GET)
     public String newBooking(Model model) throws ServiceException {
-        List<Booking> bookings = bookingService.getAllNewBooking();
-        model.addAttribute("newBooking", bookings);
-        return "admin/newbooking";
+        String page;
+        try {
+            page = null;
+            List<Booking> bookings = bookingService.getAllNewBooking();
+            model.addAttribute("newBooking", bookings);
+        } catch (ServiceException e) {
+            page = ConfigurationManager.getProperty("path.page.errorDatabase");
+            model.addAttribute("errorDatabase", MessageManager.getProperty("message.errorDatabase"));
+        }
+        return page;
+    }
+
+    @RequestMapping(value = "/allbookings", method = RequestMethod.POST)
+    public String allBooking(Model model) {
+        String page;
+        try {
+            System.out.println("cd");
+            page = ConfigurationManager.getProperty("path.page.allBookings");
+            List<Booking> bookings = bookingService.getAll();
+            model.addAttribute("allBooking", bookings);
+        } catch (ServiceException e) {
+            page = ConfigurationManager.getProperty("path.page.errorDatabase");
+            model.addAttribute("errorDatabase", MessageManager.getProperty("message.errorDatabase"));
+        }
+        return page;
     }
 }
