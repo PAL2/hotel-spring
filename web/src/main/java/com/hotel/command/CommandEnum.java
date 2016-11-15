c63c6f44276466ebe446ca3a38d161d0be9ed2ab
Alex Polegoshko <pal16@tut.by>
2016-11-15 13:53:24 +0300

    Deleted PayCommand from CommandEnum

--- web/src/main/java/com/hotel/command/CommandEnum.java
+++ web/src/main/java/com/hotel/command/CommandEnum.java
@@ -1,17 +1,11 @@
 package com.hotel.command;
 
 import com.hotel.command.client.OrderCommand;
-import com.hotel.command.client.PayCommand;
 
 public enum CommandEnum {
     ORDER {
         {
             this.command = new OrderCommand();
-        }
-    },
-    PAY {
-        {
-            this.command = new PayCommand();
         }
     };
     ActionCommand command;
